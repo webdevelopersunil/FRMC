101e5d5577ba7cd44a01f530d423ac38875fa7d6
sunil <he.kingsmen@gmail.com>
2024-02-17 18:59:23 +0530

    Block for dashboard fixed

---
+++ resources/views/includes/block.blade.php
@@ -0,0 +1,47 @@
+<div class="row">
+    <div class="col-md-12 grid-margin transparent">
+        <div class="row">
+
+            <div class="col-md-3 mb-4 stretch-card transparent">
+                <div class="card card-tale">
+                <div class="card-body">
+                    <p class="mb-4">Total Complaints</p>
+                    <p class="fs-30 mb-2">{{ $total }}</p>
+                    <p>--</p>
+                </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 mb-4 stretch-card transparent">
+                <div class="card card-dark-blue">
+                <div class="card-body">
+                    <p class="mb-4">--</p>
+                    <p class="fs-30 mb-2">-</p>
+                    <p>--</p>
+                </div>
+                </div>
+            </div>
+            
+            <div class="col-md-3 mb-4  stretch-card transparent">
+                <div class="card card-light-blue">
+                <div class="card-body">
+                    <p class="mb-4">--</p>
+                    <p class="fs-30 mb-2">--</p>
+                    <p>--</p>
+                </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 mb-4 stretch-card transparent">
+                <div class="card card-light-danger">
+                <div class="card-body">
+                    <p class="mb-4">--</p>
+                    <p class="fs-30 mb-2">--</p>
+                    <p>--</p>
+                </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
--- resources/views/nodal/dashboard.blade.php
+++ resources/views/nodal/dashboard.blade.php
@@ -30,49 +30,9 @@
           </div>
 
 
-          <div class="row">
-            <div class="col-md-12 grid-margin transparent">
-              <div class="row">
-                <div class="col-md-3 mb-4 stretch-card transparent">
-                  <div class="card card-tale">
-                    <div class="card-body">
-                      <p class="mb-4">Total Complaint</p>
-                      <p class="fs-30 mb-2">{{ $total }}</p>
-                      <p>--</p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-md-3 mb-4 stretch-card transparent">
-                  <div class="card card-dark-blue">
-                    <div class="card-body">
-                      <p class="mb-4">--</p>
-                      <p class="fs-30 mb-2">--</p>
-                      <p>--</p>
-                    </div>
-                  </div>
-                </div>
-              
-                <div class="col-md-3 mb-4  stretch-card transparent">
-                  <div class="card card-light-blue">
-                    <div class="card-body">
-                      <p class="mb-4">--</p>
-                      <p class="fs-30 mb-2">--</p>
-                      <p>--</p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-md-3 mb-4 stretch-card transparent">
-                  <div class="card card-light-danger">
-                    <div class="card-body">
-                      <p class="mb-4">--</p>
-                      <p class="fs-30 mb-2">--</p>
-                      <p>--</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <!-- Blocks section start here -->
+            @include('includes/block')
+          <!-- Blocks section ends here -->
           
 
           <div class="row">
